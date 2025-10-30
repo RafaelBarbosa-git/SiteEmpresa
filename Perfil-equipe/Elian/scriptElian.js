9ff857c9a5761c5b6379c8e2ab933a462f2903e6
Lucas Vicente <Lucas-pos>
2025-10-30 09:20:55 -0300

    fiz meu perfil e elian mecheu no dele

---
+++ Perfil-equipe/Elian/scriptElian.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(){
+    function carregarMenu() {
+                fetch('menu.html')
+                    .then(response => response.text())
+                    .then(data => {
+                        document.getElementById('menu').innerHTML = data;
+                    })
+                    .catch(error => console.error('Erro ao carregar o menu', error));
+            }
+
+    carregarMenu();
+
+    });
+
+
